50313f4debeaaa0ea12a9482e6b79512315b4072
sena-22 <senika_@naver.com>
2023-07-07 15:19:46 +0900

    chore: 경로 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,11 +3,12 @@ import {RouteState, useRouter} from '../react-router-dom'
 
 const Nav = () => {
   const navList = [
-    {to: '/', name: 'go main '},
-    {to: '/about', name: 'about'},
+    {curPath: '/about', to: '/', name: 'go main'},
+    {curPath: '/', to: '/about', name: 'about'},
   ]
+
   const pathname = useContext(RouteState)
-  const {to = '', name} = navList.find((nav) => nav.to !== pathname) || {}
+  const {to = '', name} = navList.find((nav) => nav.curPath === pathname) || {}
   const {push} = useRouter()
 
   return (
